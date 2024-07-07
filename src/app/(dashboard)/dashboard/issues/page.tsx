3f08a942ed3338af251ecc38cbceb7e368dcf848
sahil-idk <sahil.sahil@research.iiit.ac.in>
2024-07-07 11:16:25 +0530

    issue page type yet to be fixed

--- src/app/(dashboard)/dashboard/issues/page.tsx
+++ src/app/(dashboard)/dashboard/issues/page.tsx
@@ -2,7 +2,7 @@ import { IssuePage } from "@/components/component/issue-page";
 import React from "react";
 
 const Issues = () => {
-  return <IssuePage />;
+  // return <IssuePage />;
 }
 
 export default Issues;
